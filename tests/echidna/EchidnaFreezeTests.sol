1fdd988862996745f8898b25920fb997cacc62b3
RavenDream <murdoc@reserve.org>
2019-03-04 22:49:54 +0000

    test: fuzz transfer from, to, and by a frozen account

--- tests/echidna/EchidnaFreezeTests.sol
+++ tests/echidna/EchidnaFreezeTests.sol
@@ -5,16 +5,25 @@ import "ReserveDollar.sol";
 // Use the multisender.yaml to send messages from 0x1, 0x2, 0x3, but not the
 // deployer address. Non owners should be unable to change the owner
 contract EchidnaFrozenAccount is ReserveDollar {
+    bool transferredFromFrozen;
+
     constructor() public {
         minter = msg.sender;
         freezer = msg.sender;
         
         testFixtureMint(address(0x1), 123);
-        testFixtureIncreaseAllowance(address(0x1), address(0x3), 19);
         testFixtureMint(address(0x2), 1234);
         testFixtureMint(address(0x3), 12345);
+
+        // Create an allowance for transfering into the frozen address.
+        testFixtureIncreaseAllowance(address(0x2), address(0x1), 17);
+        // Create an allowance for transfering out of the frozen address.
+        testFixtureIncreaseAllowance(address(0x1), address(0x2), 19);
+
         // freeze the 0x1 address;
         testFixtureFreeze(address(0x1));
+        
+        transferredFromFrozen = false;
     }
     
     function testFixtureIncreaseAllowance(address from, address spender, uint256 addedValue) public returns (bool) {
@@ -43,14 +52,36 @@ contract EchidnaFrozenAccount is ReserveDollar {
         emit Transfer(address(0), account, value);
     }
 
+    
+    // Only allow allowance transfers into 0x3 to check that frozen account
+    // cannot send funds to a third account from an account that gives frozen
+    // account an allowance.
+    function transfer(address to, uint256 value) external returns (bool)
+    {
+        require(to != address(0x3));
+        return this.transfer(to, value);
+    }
+
+    ///////////////////////////////// 
+    
+
     function echidna_frozen_account_constant_balance() public view returns (bool) {
         return this.balanceOf(address(0x1)) == 123;
 
     }
+    
+    function echidna_prevent_frozen_msg_sender_transferFrom() public view returns (bool) {
+        return (this.balanceOf(address(0x3)) == 12345);
+    }
+    
+    function echidna_prevent_transfers_into_frozen_account() public view returns (bool) {
+        return ((this.balanceOf(address(0x2)) == 1234)
+            && (this.allowance(address(0x2), address(0x1)) == 17));
+    }
 
-    function echidna_frozen_account_unchanged_allowance() public view returns (bool) {
-        return (this.allowance(address(0x1), address(0x2)) == 0) &&
-            (this.allowance(address(0x1), address(0x3)) == 19);
+    function echidna_prevent_transfers_from_frozen_account() public view returns (bool) {
+        return ((this.balanceOf(address(0x2)) == 1234)
+            && (this.allowance(address(0x2), address(0x1)) == 17));
     }
 
     function echidna_freezer_role_unchanged() public view returns (bool) {
